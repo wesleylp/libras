2917db64e38d132a9403d625eadd8cce8a9d47f2
Wesley Passos <wesley.passos@smt.ufrj.br>
2021-04-11 14:24:50 -0300

    add helper functions to generate results

--- src/utils/results.py
+++ src/utils/results.py
@@ -1,5 +1,7 @@
 import pickle
 
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
 
@@ -28,3 +30,229 @@ def df_results(opt):
     df.sort_values(by='mean_test_score', inplace=True, ascending=False)
 
     return df
+
+
+def normalize_cfn_mtx(mtx, by='true'):
+    if by.lower() == 'true':
+        norm_factor = mtx.sum(axis=1)[:, np.newaxis]
+
+    elif by.lower() == 'pred':
+        norm_factor = mtx.sum(axis=0)
+
+    elif by.lower() == 'all':
+        norm_factor = mtx.sum()
+
+    else:
+        ValueError(f'`by` is invalid must be `true`, `pred`, or `all`: {by}')
+
+    return mtx / norm_factor
+
+
+def show_values(pc, fmt="%.2f", **kw):
+    '''
+    Heatmap with text in each cell with matplotlib's pyplot
+    Source: https://stackoverflow.com/a/25074150/395857
+    By HYRY
+    '''
+
+    pc.update_scalarmappable()
+    ax = pc.axes
+    #ax = pc.axes# FOR LATEST MATPLOTLIB
+    #Use zip BELOW IN PYTHON 3
+    for p, color, value in zip(pc.get_paths(), pc.get_facecolors(), pc.get_array()):
+        x, y = p.vertices[:-2, :].mean(0)
+        if np.all(color[:3] > 0.5):
+            color = (0.0, 0.0, 0.0)
+        else:
+            color = (1.0, 1.0, 1.0)
+        ax.text(x, y, fmt % value, ha="center", va="center", color=color, **kw)
+
+
+def cm2inch(*tupl):
+    '''
+    Specify figure size in centimeter in matplotlib
+    Source: https://stackoverflow.com/a/22787457/395857
+    By gns-ank
+    '''
+    inch = 2.54
+    if type(tupl[0]) == tuple:
+        return tuple(i / inch for i in tupl[0])
+    else:
+        return tuple(i / inch for i in tupl)
+
+
+def heatmap(AUC,
+            title,
+            xlabel,
+            ylabel,
+            xticklabels,
+            yticklabels,
+            figure_width=40,
+            figure_height=20,
+            correct_orientation=False,
+            cmap='RdBu'):
+    '''
+    Inspired by:
+    - https://stackoverflow.com/a/16124677/395857
+    - https://stackoverflow.com/a/25074150/395857
+    '''
+
+    # Plot it out
+    fig, ax = plt.subplots()
+    #c = ax.pcolor(AUC, edgecolors='k', linestyle= 'dashed', linewidths=0.2, cmap='RdBu', vmin=0.0, vmax=1.0)
+    c = ax.pcolor(AUC,
+                  edgecolors='k',
+                  linestyle='dashed',
+                  linewidths=0.2,
+                  cmap=cmap,
+                  vmin=0.0,
+                  vmax=1.0)
+
+    # put the major ticks at the middle of each cell
+    ax.set_yticks(np.arange(AUC.shape[0]) + 0.5, minor=False)
+    ax.set_xticks(np.arange(AUC.shape[1]) + 0.5, minor=False)
+
+    # set tick labels
+    #ax.set_xticklabels(np.arange(1,AUC.shape[1]+1), minor=False)
+    ax.set_xticklabels(xticklabels, minor=False)
+    ax.set_yticklabels(yticklabels, minor=False)
+
+    # set title and x/y labels
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+
+    # Remove last blank column
+    plt.xlim((0, AUC.shape[1]))
+
+    # Turn off all the ticks
+    ax = plt.gca()
+    for t in ax.xaxis.get_major_ticks():
+        #         t.tick1On = False
+        #         t.tick2On = False
+        t.tick1line.set_visible(False)
+        t.tick2line.set_visible(False)
+    for t in ax.yaxis.get_major_ticks():
+        #         t.tick1On = False
+        #         t.tick2On = False
+        t.tick1line.set_visible(False)
+        t.tick2line.set_visible(False)
+
+    # Add color bar
+    plt.colorbar(c)
+
+    # Add text in each cell
+    show_values(c)
+
+    # Proper orientation (origin at the top left instead of bottom left)
+    if correct_orientation:
+        ax.invert_yaxis()
+        ax.xaxis.tick_top()
+        ax.xaxis.set_label_position('top')
+
+    # resize
+    fig = plt.gcf()
+    #fig.set_size_inches(cm2inch(40, 20))
+    #fig.set_size_inches(cm2inch(40*4, 20*4))
+    fig.set_size_inches(cm2inch(figure_width, figure_height))
+
+
+def plot_classification_report(classification_report,
+                               title='Classification report',
+                               cmap='RdBu',
+                               figure_width=25,
+                               correct_orientation=False,
+                               quiet=True):
+    '''
+    Plot scikit-learn classification report.
+    Extension based on https://stackoverflow.com/a/31689645/395857
+    '''
+    # lines = classification_report.split('\n')
+
+    # classes = []
+    # plotMat = []
+    # support = []
+    # class_names = []
+    # for line in lines[2:(len(lines) - 4)]:
+    #     t = line.strip().split()
+    #     if len(t) < 2: continue
+    #     classes.append(t[0])
+    #     v = [float(x) for x in t[1:len(t) - 1]]
+    #     support.append(int(t[-1]))
+    #     class_names.append(t[0])
+
+    #     if not quiet:
+    #         print(v)
+
+    #     plotMat.append(v)
+
+    if isinstance(classification_report, str):
+        classes, plotMat, support, class_names = read_classification_report(classification_report,
+                                                                            quiet=quiet)
+    elif isinstance(classification_report, dict):
+        classes = classification_report['classes']
+        plotMat = classification_report['plotMat']
+        support = classification_report['support']
+        class_names = classification_report['class_names']
+
+    else:
+        ValueError(f'classification_report must be `str` or `dict`: {type(classification_report)}')
+
+    if not quiet:
+        print('plotMat: {0}'.format(plotMat))
+        print('support: {0}'.format(support))
+
+    xlabel = 'Metrics'
+    ylabel = 'Classes'
+    xticklabels = ['Precision', 'Recall', 'F1-score']
+    #     yticklabels = ['{0} ({1})'.format(class_names[idx], sup) for idx, sup  in enumerate(support)]
+    yticklabels = ['{0}'.format(class_names[idx]) for idx, _ in enumerate(support)]
+    #     figure_width = 25
+    figure_height = len(class_names) + 7
+    #     correct_orientation = False
+    heatmap(np.array(plotMat),
+            title,
+            xlabel,
+            ylabel,
+            xticklabels,
+            yticklabels,
+            figure_width,
+            figure_height,
+            correct_orientation,
+            cmap=cmap)
+
+
+def read_classification_report(classification_report, quiet=True):
+    lines = classification_report.split('\n')
+
+    classes = []
+    Mat = []
+    support = []
+    class_names = []
+    for line in lines[2:(len(lines) - 4)]:
+        t = line.strip().split()
+        if len(t) < 2: continue
+        classes.append(t[0])
+        v = [float(x) for x in t[1:len(t) - 1]]
+        support.append(int(t[-1]))
+        class_names.append(t[0])
+
+        if not quiet:
+            print(v)
+
+        Mat.append(v)
+
+    return classes, Mat, support, class_names
+
+
+def mean_classification_report(classification_reports):
+    all_cls_reports = []
+
+    for key, cls_report in classification_reports.items():
+        classes, Mat, support, class_names = read_classification_report(classification_reports[key])
+        all_cls_reports.append(Mat)
+
+    Mat_mean = np.mean(all_cls_reports, axis=0)
+    Mat_std = np.std(all_cls_reports, axis=0)
+
+    return Mat_mean, Mat_std
