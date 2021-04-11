cc591ab19ba218d300ac2f4d172b0653af9262cb
Wesley Passos <wesley.passos@smt.ufrj.br>
2021-04-11 14:25:33 -0300

    keep search space in a single file

---
+++ src/utils/search_spaces.py
@@ -0,0 +1,46 @@
+import numpy as np
+from scipy.stats import loguniform
+from sklearn.decomposition import TruncatedSVD
+from sklearn.pipeline import Pipeline
+from sklearn.svm import SVC
+from skopt.space import Categorical, Integer, Real
+
+# pipeline class is used as estimator to enable
+# search over different model types
+base_pipe = Pipeline([('model', SVC())])
+base_pipe_reduction = Pipeline([('reduction', TruncatedSVD()), ('model', SVC())])
+
+SVD_space_bayes = {
+    'reduction': Categorical([
+        TruncatedSVD(random_state=0),
+    ]),
+    'reduction__n_components': Integer(2, 150),
+}
+
+SVC_space_bayes = {
+    'model': Categorical([SVC()]),
+    'model__C': Real(1e-6, 1e+6, prior='log-uniform'),
+    'model__gamma': Real(1e-6, 1e+1, prior='log-uniform'),
+    'model__degree': Integer(1, 8),
+    'model__kernel': Categorical(['linear', 'poly', 'rbf'])
+}
+
+SVD_space = {
+    'reduction': [
+        TruncatedSVD(random_state=0),
+    ],
+    'reduction__n_components': np.arange(2, 150, dtype=int),
+}
+
+SVC_space = {
+    'model': [
+        SVC(),
+    ],
+    'model__C': loguniform(
+        1e-6,
+        1e+6,
+    ),
+    'model__gamma': loguniform(1e-6, 1e+1),
+    'model__degree': np.arange(1, 8, dtype=int),
+    'model__kernel': ['linear', 'poly', 'rbf'],
+}
